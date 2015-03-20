3c50527b9d8a6377d719b919df365041c2795b63
Drew Marsh <drub0y@hotmail.com>
2015-03-20 15:28:22 -0700

    Intial solution/project creation/configuration.

---
+++ Obvs.AzureServiceBus.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Obvs.AzureServiceBus.Tests")]
+[assembly: AssemblyDescription("Test for the Azure ServiceBus support for the Obvs framework.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Drew Marsh")]
+[assembly: AssemblyProduct("Obvs.AzureServiceBus")]
+[assembly: AssemblyCopyright("Copyright © Drew Marsh 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.0.*")]
---
+++ Obvs.AzureServiceBus/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("Obvs.AzureServiceBus")]
+[assembly: AssemblyDescription("Azure ServiceBus support for the Obvs framework.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Drew Marsh")]
+[assembly: AssemblyProduct("Obvs.AzureServiceBus")]
+[assembly: AssemblyCopyright("Copyright © Drew Marsh 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyInformationalVersion("0.1.0-alpha")]
