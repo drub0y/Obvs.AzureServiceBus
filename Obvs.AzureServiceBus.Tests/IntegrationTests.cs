f83048e9c2f4ed73b7c00b2044a86244071db0ca
Drew Marsh <drub0y@hotmail.com>
2015-04-08 23:37:11 -0700

    Adding initial integration test.
       * Need to do a LOT more work on these, but it's a start.

---
+++ Obvs.AzureServiceBus.Tests/IntegrationTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Obvs.AzureServiceBus.Configuration;
+using Obvs.Serialization.Json;
+using Obvs.Types;
+using Xunit;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace Obvs.AzureServiceBus.Tests
+{
+    public class IntegrationTests
+    {
+        [Fact]
+        public async Task Test()
+        {
+            IServiceBus serviceBus = ServiceBus.Configure()
+                .WithAzureServiceBusEndpoint<ITestMessage>()
+                .Named("Test")
+                .WithConnectionString("TODO: load this from a JSON file that is not checked into the repo")
+                .UsingQueueFor<ICommand>("obvs-test-commands")
+                .UsingQueueFor<IRequest>("obvs-test-requests")
+                .UsingQueueFor<IResponse>("obvs-test-responses")
+                .UsingTopicFor<IEvent>("obvs-test-events")
+                .UsingSubscriptionFor<IEvent>("obvs-test-events/subscription")
+                .SerializedWith(new JsonMessageSerializer(), new JsonMessageDeserializerFactory())
+                .FilterMessageTypeAssemblies("Obvs.AzureServiceBus.Tests")
+                .AsClientAndServer()
+                .Create();
+
+            await serviceBus.SendAsync(new TestCommand
+            {
+                Data = "Hello Azure SB!"
+            });
+
+            TestCommand command = await serviceBus.Commands.Timeout(TimeSpan.FromSeconds(5)).OfType<TestCommand>().FirstOrDefaultAsync();
+
+            command.Should().NotBeNull();
+        }
+    }
+
+    public interface ITestMessage : IMessage
+    {
+    }
+
+    public class TestCommand : ITestMessage, ICommand
+    {
+        public string Data
+        {
+            get;
+            set;
+        }
+    }
+}
