d63de5f498d436980eb7d84f509e05805ca18c23
Drew Marsh <drub0y@hotmail.com>
2015-05-07 17:41:19 -0700

    Updating and enhancing integration tests.
    Added ability to load service bus connection string for integration tests from either an environment variable or a JSON file in the top directory which will be ignored by the .gitignore file so nobody accidentally commits their private connection string.

--- Obvs.AzureServiceBus.Tests/IntegrationTests.cs
+++ Obvs.AzureServiceBus.Tests/IntegrationTests.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Obvs.AzureServiceBus.Configuration;
-using Obvs.Serialization.Json;
+using Obvs.Serialization.Json.Configuration;
 using Obvs.Types;
 using Xunit;
 using System.Reactive;
@@ -8,44 +8,88 @@ using System.Reactive.Linq;
 using System.Reactive.Subjects;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Newtonsoft.Json;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Diagnostics;
 
 namespace Obvs.AzureServiceBus.Tests
 {
     public class IntegrationTests
     {
+        private static readonly string ServiceBusConnectionString = GetServiceBusConnectionString();
+        
         [Fact]
         public async Task Test()
         {
             IServiceBus serviceBus = ServiceBus.Configure()
-                .WithAzureServiceBusEndpoint<ITestMessage>()
+                .WithAzureServiceBusEndpoint<TestMessage>()
                 .Named("Test")
-                .WithConnectionString("TODO: load this from a JSON file that is not checked into the repo")
-                .UsingQueueFor<ICommand>("obvs-test-commands")
-                .UsingQueueFor<IRequest>("obvs-test-requests")
-                .UsingQueueFor<IResponse>("obvs-test-responses")
-                .UsingTopicFor<IEvent>("obvs-test-events")
-                .UsingSubscriptionFor<IEvent>("obvs-test-events/subscription")
-                .SerializedWith(new JsonMessageSerializer(), new JsonMessageDeserializerFactory())
+                .WithConnectionString(IntegrationTests.ServiceBusConnectionString)
+                .UsingQueueFor<ICommand>("obvs-test-commands", MessageReceiveMode.ReceiveAndDelete, MessagingEntityCreationOptions.CreateIfDoesntExist | MessagingEntityCreationOptions.CreateAsTemporary | MessagingEntityCreationOptions.RecreateExistingTemporary)
+                .UsingQueueFor<IRequest>("obvs-test-requests", MessageReceiveMode.ReceiveAndDelete, MessagingEntityCreationOptions.CreateIfDoesntExist | MessagingEntityCreationOptions.CreateAsTemporary | MessagingEntityCreationOptions.RecreateExistingTemporary)
+                .UsingQueueFor<IResponse>("obvs-test-responses", MessageReceiveMode.ReceiveAndDelete, MessagingEntityCreationOptions.CreateIfDoesntExist | MessagingEntityCreationOptions.CreateAsTemporary | MessagingEntityCreationOptions.RecreateExistingTemporary)
+                .UsingTopicFor<IEvent>("obvs-test-events", MessagingEntityCreationOptions.CreateIfDoesntExist | MessagingEntityCreationOptions.CreateAsTemporary | MessagingEntityCreationOptions.RecreateExistingTemporary)
+                .UsingSubscriptionFor<IEvent>("obvs-test-events", "obvs-test-subscription", MessageReceiveMode.ReceiveAndDelete, MessagingEntityCreationOptions.CreateIfDoesntExist | MessagingEntityCreationOptions.CreateAsTemporary | MessagingEntityCreationOptions.RecreateExistingTemporary)
+                .SerializedAsJson()
                 .FilterMessageTypeAssemblies("Obvs.AzureServiceBus.Tests")
                 .AsClientAndServer()
                 .Create();
 
+            string commandId = Guid.NewGuid().ToString("n");
+
             await serviceBus.SendAsync(new TestCommand
             {
+                Id = commandId,
                 Data = "Hello Azure SB!"
             });
 
-            TestCommand command = await serviceBus.Commands.Timeout(TimeSpan.FromSeconds(5)).OfType<TestCommand>().FirstOrDefaultAsync();
+            TestCommand command = await serviceBus.Commands
+                .OfType<TestCommand>()
+                .Where(c => c.Id == commandId)
+                .Timeout(TimeSpan.FromSeconds(5))
+                .FirstOrDefaultAsync();
 
             command.Should().NotBeNull();
         }
+
+        private static string GetServiceBusConnectionString()
+        {
+            string result = Environment.GetEnvironmentVariable("Obvs.AzureServiceBus.Tests.IntegrationTests.ServiceBusConnectionString");
+
+            if(string.IsNullOrWhiteSpace(result))
+            {
+                try
+                {
+                    dynamic configuration = new JsonSerializer().Deserialize(new JsonTextReader(new StreamReader(@"..\..\..\IntegrationTestsConfig.json")));
+
+                    result = configuration.ServiceBusConnectionString;
+                }
+                catch(FileNotFoundException)
+                {
+                    Debug.WriteLine("Didn't find JSON configuration file.");
+                }
+            }
+
+            if(string.IsNullOrWhiteSpace(result))
+            {
+                throw new Exception("No service bus connection string has been configured.");
+            }
+
+            return result;
+        }
     }
 
-    public interface ITestMessage : IMessage
+    public class TestMessage : IMessage
     {
+        public string Id
+        {
+            get;
+            set;
+        }
     }
 
-    public class TestCommand : ITestMessage, ICommand
+    public class TestCommand : TestMessage, ICommand
     {
         public string Data
         {
