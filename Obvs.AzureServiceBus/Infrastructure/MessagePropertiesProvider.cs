5c058f2784f36afe2b86ea311d8476bed2307302
Drew Marsh <drub0y@hotmail.com>
2016-07-18 15:21:39 -0700

    Initial implementation of message properties.
     * Surface API of Get[Incoming|Outgoing]MessageProperties extenstion methods and I[Incoming|Outgoing]MessageProperties interfaces
     * MessagePropertiesProvider implementation that follows same design as peek lock control stuff.

---
+++ Obvs.AzureServiceBus/Infrastructure/MessagePropertiesProvider.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Obvs.AzureServiceBus.Infrastructure
+{
+    internal static class MessagePropertiesProvider
+    {
+        public static IMessagePropertiesProvider Default;
+    }
+
+    internal interface IMessagePropertiesProvider
+    {
+        IIncomingMessageProperties GetIncomingMessageProperties(object message);
+        IOutgoingMessageProperties GetOutgoingMessageProperties(object message);
+    }
+
+    internal sealed class DefaultBrokeredMessagePropertiesProvider : IMessagePropertiesProvider
+    {
+        private IMessageBrokeredMessageTable _messageBrokeredMessageTable;
+
+        public DefaultBrokeredMessagePropertiesProvider(IMessageBrokeredMessageTable messageBrokeredMessageTable)
+        {
+            _messageBrokeredMessageTable = messageBrokeredMessageTable;
+        }
+
+        public IIncomingMessageProperties GetIncomingMessageProperties(object message) => new DefaultBrokeredMessageIncomingMessageProperties(_messageBrokeredMessageTable.GetBrokeredMessageForMessage(message));
+
+        public IOutgoingMessageProperties GetOutgoingMessageProperties(object message) => new DefaultBrokeredMessageOutgoingMessageProperties(_messageBrokeredMessageTable.GetBrokeredMessageForMessage(message));
+    }
+
+    public interface IIncomingMessageProperties
+    {
+        int DeliveryCount
+        {
+            get;
+        }
+    }
+
+    public interface IOutgoingMessageProperties
+    {
+        DateTime ScheduledEnqueueTimeUtc
+        {
+            get;
+            set;
+        }
+
+        TimeSpan TimeToLive
+        {
+            get;
+            set;
+        }
+    }
+
+    internal struct DefaultBrokeredMessageIncomingMessageProperties : IIncomingMessageProperties
+    {
+        private BrokeredMessage _brokeredMessage;
+
+        public DefaultBrokeredMessageIncomingMessageProperties(BrokeredMessage brokeredMessage)
+        {
+            _brokeredMessage = brokeredMessage;
+        }
+
+        public int DeliveryCount
+        {
+            get
+            {
+                return _brokeredMessage.DeliveryCount;
+            }
+        }
+    }
+
+    internal struct DefaultBrokeredMessageOutgoingMessageProperties : IOutgoingMessageProperties
+    {
+        private BrokeredMessage _brokeredMessage;
+
+        public DefaultBrokeredMessageOutgoingMessageProperties(BrokeredMessage brokeredMessage)
+        {
+            _brokeredMessage = brokeredMessage;
+        }
+
+        public DateTime ScheduledEnqueueTimeUtc
+        {
+            get
+            {
+                return _brokeredMessage.ScheduledEnqueueTimeUtc;
+            }
+            set
+            {
+                _brokeredMessage.ScheduledEnqueueTimeUtc = value;
+            }
+        }
+
+        public TimeSpan TimeToLive
+        {
+            get
+            {
+                return _brokeredMessage.TimeToLive;
+            }
+
+            set
+            {
+                _brokeredMessage.TimeToLive = value;
+            }
+        }
+    }
+}
---
+++ Obvs.AzureServiceBus/MessagePropertiesMessageExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Obvs.AzureServiceBus.Infrastructure;
+
+namespace Obvs.AzureServiceBus
+{
+    public static class MessagePropertiesMessageExtensions
+    {
+        public static IOutgoingMessageProperties GetOutgoingMessageProperties<TMessage>(this TMessage message) where TMessage : class
+        {
+            if(message == null) throw new ArgumentNullException(nameof(message));
+
+            IMessagePropertiesProvider configuredMessagePropertyProvider = MessagePropertiesProvider.Default;
+
+            if(configuredMessagePropertyProvider == null)
+            {
+                throw new InvalidOperationException($"No {nameof(IMessagePropertiesProvider)} has been configured.");
+            }
+
+            return configuredMessagePropertyProvider.GetOutgoingMessageProperties(message);
+        }
+
+        public static IIncomingMessageProperties GetIncomingMessageProperties<TMessage>(this TMessage message) where TMessage : class
+        {
+            if(message == null) throw new ArgumentNullException(nameof(message));
+
+            IMessagePropertiesProvider configuredMessagePropertyProvider = MessagePropertiesProvider.Default;
+
+            if(configuredMessagePropertyProvider == null)
+            {
+                throw new InvalidOperationException($"No {nameof(IMessagePropertiesProvider)} has been configured.");
+            }
+
+            return configuredMessagePropertyProvider.GetIncomingMessageProperties(message);
+        }
+    }
+}
